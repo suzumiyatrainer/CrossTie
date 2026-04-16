b5a8229c63f2e6a6e9100754d53a3eb57883703b
suzumiyatrainer <suzumiya.katsuhiro@gmail.com>
2026-04-16 21:39:50 +0900

    1.0.0-Alpha5(RC3)

--- src/main/java/net/suzumiya/crosstie/mixins/rtm/TileEntityLargeRailCoreMixin.java
+++ src/main/java/net/suzumiya/crosstie/mixins/rtm/TileEntityLargeRailCoreMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.util.AxisAlignedBB;
 import net.suzumiya.crosstie.CrossTie;
 import net.suzumiya.crosstie.config.CrossTieConfig;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -14,20 +15,35 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(targets = "jp.ngt.rtm.rail.TileEntityLargeRailCore", remap = false)
 public abstract class TileEntityLargeRailCoreMixin extends TileEntity {
 
+    @Unique
+    private static final int CROSSTIE_FORCE_RENDER_CHUNKS = 2;
+
+    @Unique
+    private static final double CROSSTIE_FORCE_RENDER_DISTANCE_BLOCKS = CROSSTIE_FORCE_RENDER_CHUNKS * 16.0D;
+
     @Override
     @SideOnly(Side.CLIENT)
     public double getMaxRenderDistanceSquared() {
         int renderDistance = CrossTie.proxy.getClientRenderDistance();
         if (renderDistance > 0) {
-            double blockDistance = renderDistance * 16.0D;
+            double blockDistance = Math.max(renderDistance, CROSSTIE_FORCE_RENDER_CHUNKS) * 16.0D;
             return blockDistance * blockDistance;
         }
-        return super.getMaxRenderDistanceSquared();
+        return CROSSTIE_FORCE_RENDER_DISTANCE_BLOCKS * CROSSTIE_FORCE_RENDER_DISTANCE_BLOCKS;
     }
 
     @Inject(method = "getRenderBoundingBox", at = @At("HEAD"), cancellable = true, remap = false)
     @SideOnly(Side.CLIENT)
     private void crosstie$fixAngelicaRailCulling(CallbackInfoReturnable<AxisAlignedBB> cir) {
+        net.minecraft.entity.Entity player = CrossTie.proxy.getClientPlayer();
+        if (player != null) {
+            double forceDistanceSq = CROSSTIE_FORCE_RENDER_DISTANCE_BLOCKS * CROSSTIE_FORCE_RENDER_DISTANCE_BLOCKS;
+            if (player.getDistanceSq(this.xCoord + 0.5D, this.yCoord + 0.5D, this.zCoord + 0.5D) <= forceDistanceSq) {
+                cir.setReturnValue(INFINITE_EXTENT_AABB);
+                return;
+            }
+        }
+
         if (CrossTieConfig.fixAngelicaRailCulling) {
             cir.setReturnValue(INFINITE_EXTENT_AABB);
         }
