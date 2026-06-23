2f12f7078900e199e62a460fee40ba4c31490de7
suzumiyatrainer <suzumiya.katsuhiro@gmail.com>
2026-06-23 22:10:21 +0900

    1.0.0-Alpha7(beta2.1)

--- src/main/java/net/suzumiya/crosstie/mixins/rtm/EntityTrainClientSmoothingMixin.java
+++ src/main/java/net/suzumiya/crosstie/mixins/rtm/EntityTrainClientSmoothingMixin.java
@@ -4,7 +4,7 @@ import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.Inject;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
