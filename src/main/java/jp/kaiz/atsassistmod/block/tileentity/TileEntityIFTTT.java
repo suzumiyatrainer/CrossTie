0c65b7e16d6974dcafd3ab3caf0e2bc3b9bf233d
suzumiyatrainer <suzumiya.katsuhiro@gmail.com>
2026-07-10 22:50:55 +0900

    fix -attempt No2.2

---
+++ src/main/java/jp/kaiz/atsassistmod/block/tileentity/TileEntityIFTTT.java
@@ -0,0 +1,15 @@
+package jp.kaiz.atsassistmod.block.tileentity;
+
+import net.minecraft.tileentity.TileEntity;
+
+/**
+ * Compile-only stub class for ATSAssistMod compatibility.
+ * This class is only used to satisfy the compiler when compiling Mixins.
+ * It will not be packaged into the final release JAR, or even if it is,
+ * the actual runtime class from ATSAssistMod will take precedence.
+ */
+public class TileEntityIFTTT extends TileEntity {
+    public int getTick() {
+        return 0;
+    }
+}
